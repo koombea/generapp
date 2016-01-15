ef2c1ec08914eb11bbadab644ad174a471e49209
Gustavo Bazan <gustavo.bazan@koombea.com>
2016-01-15 16:28:11 -0500

    Update default templates
    Remove staging from database configuration
    Document Gemfile
    Add coverage for circleci
    Code style improvements

--- templates/spec/rails_helper.rb
+++ templates/spec/rails_helper.rb
@@ -1,7 +1,16 @@
-require 'simplecov'
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
+# Coverage
+require 'simplecov'
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start 'rails'
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
