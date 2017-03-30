aab12770b5e9d3239fd2c2a1057b3d7c38de4a42
Gustavo Bazan <gustavo@swiftcomply.ie>
2017-03-30 09:39:44 +0100

    Bump version to 0.4.0

--- lib/generapp/version.rb
+++ lib/generapp/version.rb
@@ -9,5 +9,5 @@ module Generapp
   # Default Ruby Version
   RUBY_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip
   # Gem Version
-  VERSION = '0.3.2'
+  VERSION = '0.4.0'
 end
